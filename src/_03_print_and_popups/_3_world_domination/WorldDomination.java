e6792bf7b723268b30846a3a484dc3ec35a5fb76
student <student@ip-172-31-61-77.us-west-2.compute.internal>
2021-04-06 18:31:11 -0700

    yes hahahahaha

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -10,7 +10,7 @@ public class WorldDomination {
 		// 1. Ask the user if they know how to write code.
 		String input = JOptionPane.showInputDialog("Do you know how to code?");
 		// 2. If they say "yes", tell them they will rule the world.
-		if (input == "yes") {
+		if (input .equals ("yes")) {
 			JOptionPane.showMessageDialog(null,"You will rule the world");
 		} else {
 			JOptionPane.showMessageDialog(null, "Sign up for classes at the League");
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,16 +1,33 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
-		
+		int score = 0;
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		String input = JOptionPane.showInputDialog("What is 1 plus 1?");
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+		if (input .equals ("2")) {
+			JOptionPane.showMessageDialog(null,"Correct");
+			score ++;
+		} else {
+			JOptionPane.showMessageDialog(null, "WRONG");
+			score--;
+		}
+		 input = JOptionPane.showInputDialog("What is 2 plus 2?");
 				// 2.  Ask the user a question 
-				
+			if(input .equals("4")){
+				JOptionPane.showMessageDialog(null,"Correct");
+				score++;
+			} else {
+				JOptionPane.showMessageDialog(null, "WRONG");
+				score--;
+			}
+			 JOptionPane.showMessageDialog(null,"Your score is :"+score);
+	}
 				// 3.  Use an if statement to check if their answer is correct
-				
+			
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
@@ -20,4 +37,4 @@ public class QuizGame {
 		// After all the questions have been asked, tell the user their final score 
 		
 	}
-}
+
